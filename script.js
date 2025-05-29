c096de56d44248b718640ecbc2c67bd1424498d0
isaasz18 <isabeli.gaspar@escola.pr.gov.br>
2025-05-29 10:48:34 -0300

    aula3/29maio

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const botaodeacessibiconstlidade = document.getElementById ('botao-acessibilidade')
+    const botaodeacessibiconstlidade = document.getElementById ('opcao-acessibilidade')
+
+    botaodeacessibilidade.addEventListener('click', function( {
+        botaoDeacessibilidade.classList.toogle
+            
+                
+    
